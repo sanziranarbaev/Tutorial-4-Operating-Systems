369693cbef15a111e6b4c44eeb2f7c82c8b7477b
sanziranarbaev <91101806+sanziranarbaev@users.noreply.github.com>
2025-02-11 17:35:45 -0500

    Update players.c

--- players.c
+++ players.c
@@ -1,7 +1,9 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Arno Nazarian 100868853
+                        Hasaan Rashidi 100876631
+                        Sanzir Anarbaev 100704172 >
  * All rights reserved.
  *
  */
@@ -13,12 +15,22 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
-    return false;
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            return true;  // Player found
+        }
+    }
+    return false;  // Player not found
 }
 
-// Go through the list of players and update the score for the 
-// player given their name
+// Go through player list and update the score for given player 
+
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            players[i].score += score;  // Add score to the player's current score
+            break;  // Exit loop once the player is found and score is updated
+        }
+    }
 }
